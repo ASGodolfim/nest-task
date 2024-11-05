c45f95a187dc38e862a0441f5325b6566e673204
ASGodolfim <stonegodolfim@gmail.com>
2024-11-05 14:53:21 -0300

    feat create task

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { Task } from './task.model';
+import { title } from 'process';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('task')
 export class TaskController {
@@ -10,5 +12,10 @@ export class TaskController {
     @Get()
     getAllTasks(): Task[] {
         return this.taskService.getAllTasks(); 
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto): Task{
+        return this.taskService.createTask(createTaskDto);
     }
 }
